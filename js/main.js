e2278ab530136a87e420b0728bdb828d30a92df1
Phil N <phi.nguyen@laverne.edu>
2017-04-17 14:31:41 -0700

    Add slide for hero who am i

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var $heroAboutMe = $('.hero-about-me');
+console.log($heroAboutMe);
+var scrollState = "top";
+
 $(window).scroll(function() {
-  console.log('scrolled');
+  var scrollPos = $(window).scrollTop();
+
+  //this makes sure animation only run ONCE!
+  if ( (scrollPos !==0) && (scrollState === 'top') ) {
+    console.log('runnin!');
+    $heroAboutMe.animate(
+      {
+
+      }
+    );
+
+    $heroAboutMe.slideDown(2000);
+    //make sure to track scroll state
+    scrollState = 'scrolled';
+  }
+  // console.log(scrollPos);
+  // console.log('scrolled');
+
 });
